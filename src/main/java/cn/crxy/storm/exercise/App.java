be9ad615bbb68ca8e65c23b69fd980f6c1b976cc
xueyanyun <xueyanyun.1107@163.com>
2015-04-13 22:26:20 +0800

    初始化

---
+++ src/main/java/cn/crxy/storm/exercise/App.java
@@ -0,0 +1,13 @@
+package cn.crxy.storm.exercise;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
